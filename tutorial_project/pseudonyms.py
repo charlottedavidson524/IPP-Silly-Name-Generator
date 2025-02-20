60af1fe3820e70a26c4603b0a9a4cee2e0d0e52e
charlottedavidson524 <charlottedavidson256@gmail.com>
2025-02-20 23:32:15 +0000

    Bulk of program written.

---
+++ tutorial_project/pseudonyms.py
@@ -0,0 +1,28 @@
+import sys, random
+
+print("Welcome to the Benedict Cumberbatch name picker.' \n")
+print("A name fit for a Hollywood star.:\n\n")
+
+first = ('Blubberbutt', 'Benedict', 'Benadryl', 'Benchthis', 'Bonapart', 'Brokenbrick', 'Boppinstick', 'Benefit', 
+         'Scissorkick', 'Backitup', 'Beezlebub', 'Burgerking', 'Blenderdick', 'Billiardball', 'Guiltyverdick', 
+         'Beanbag', 'Carrotstick', 'Brodyquest', 'Beachbody', 'Bendylick', 'Baseballmitt', 'Bedbug', 'Bunsenburner',
+         'Bengaltiger', 'Budapest', 'Handpicked')
+
+last = ('Calldispatch', 'Comedicmismatch', 'Cunningscratch', 'Cumberfinch', 'Humperdinck', 'Lumberlatch', 'Flubbercrack',
+        'Cumberbatch', 'Bandersnatch', 'Cuttlefish', 'Slumberbelch', 'Cupboardlatch', 'Combyourthatch', 'Thundermunch',
+        'Cricketbat', 'Johnnycash', 'Comelycat', 'Custardbath', 'Thundercats', 'Numbercrunch', 'Luckycatch', 'Covertrack',
+        'Uptoscratch', 'Compasstrap', 'Chunkybap', 'Candygram')
+
+while True:
+    first_name = random.choice(first)
+
+    last_name = random.choice(last)
+
+    print("\n\n")
+    print("{} {}".format(first_name, last_name), file=sys.stderr)
+    print("\n\n")
+    
+    try_again = input("\n\nTry again? (Press Enter else n to quit)\n")
+    if try_again.lower() == "n":
+        break
+
